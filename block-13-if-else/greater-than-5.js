53a1cc317edd7924244f30fde5e69e6b20ceb652
Stewart Valdez <valdezstewart@gmail.com>
2023-04-21 18:59:17 -0700

    Updated greater-than-5.js

---
+++ block-13-if-else/greater-than-5.js
@@ -0,0 +1,50 @@
+// Below there will be examples utilizing if/else statements.
+// We are looking to get a script tag to print true if both parameters are greater or equal to 5.
+
+// Here is an example of num1 and num2 being greater than 5. //true
+let num1 = 5;
+let num2 = 6;
+if (num1 + num2 >= 5) {
+    console.log("true");
+}
+
+// Here is an example of the two variables being greater than 5. //true
+let num1 = 10;
+let num2 = 11;
+if (num1 + num2 >= 50) {
+    console.log("true");
+}
+
+// Here is an example of a false statement based on the follow variables. //false
+let num1 = 0;
+let num2= 0;
+if (num1 + num2 >= 5) {
+    console.log("true");
+} else {
+        console.log("false");
+    }
+
+// Another example of 2 variables giving a false output. //false
+let num1 = 1000;
+let num2 = -1000;
+if (num1 + num2 >= 5) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+// We here have an example of a false output with the values of 6 and 4. //false
+let num1 = 6;
+let num2 = 4;
+if (num1 - num2 >= 5){
+    console.log("true")
+} else {
+    console.log("false");
+}
+
+// A final example showing the two variables add up a sum to the better greater than or equal to 5. //true
+let num1 = 5;
+let num2 = 5:
+if (num1 + num2 >= 5){
+    console.log("true");
+}
