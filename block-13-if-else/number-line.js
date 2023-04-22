881b79231b4067fe5f5407e779954d1564b3e0d9
Stewart Valdez <valdezstewart@gmail.com>
2023-04-21 18:43:45 -0700

    Updated number-line.js

---
+++ block-13-if-else/number-line.js
@@ -0,0 +1,42 @@
+// Below will be results using if/else statements to categorize the sum of different numbers.
+
+let num1 = 50;
+let num2 = 51;
+if(num1 + num2 >= 100){
+    console.log("101 is greater than 100.");// The result produces a true statement.
+}
+
+// Showing a greater than value sum.
+let num1 = 99;
+let num2 = -2;
+if(num1 + num2 > 0){
+    console.log("97 is greater than 0.");
+}
+
+// Displaying another greater than value.
+let num1 = 0;
+let num2 = 101;
+if (num1 + num2 > 100){
+    console.log("101 is greater than 100.");
+}
+
+// Showing an is equal to operator by printing 0.
+let num1 = 500;
+let num2 = -500;
+if(num1 + num2 === 0){
+    console.log("0 is equal to 0.");
+}
+
+// Defining a negative number is a negative number.
+let num1 = -1000;
+let num2 = 0;
+if(num1 + num2 === -1000){
+    console.log("-1000 is a negative number.")
+}
+
+// Another example of a negative number through an is equal to operator.
+let num1 = -5;
+let num2 = 0;
+if(num1 + num2 === -5){
+    console.log("-5 is a negative number.");
+}
