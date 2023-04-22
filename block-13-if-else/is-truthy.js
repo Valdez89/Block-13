902af0d14e8be25ff1fe2fc443bbb90e170184b3
Stewart Valdez <valdezstewart@gmail.com>
2023-04-21 18:26:40 -0700

    Updating code.

---
+++ block-13-if-else/is-truthy.js
@@ -0,0 +1,44 @@
+// Below we will be using if/else statements to print both truthy and falsy results.
+
+// Here is a boolean with a true result.
+
+let aString = "I am a string";
+if(aString){
+    console.log("True")// Output is designed be true.
+}
+
+
+// Below we show an if/else boolean statement with a falsy result.
+
+let aString ="I am a string";
+if(astring){
+    console.log("True");
+} else {
+    console.log("The boolean value false is falsy.");
+}
+
+// Here is a const that prints a null falsy reponse.
+
+const nothing = null;
+if(nothing){
+    console.log("The null value is falsy.");
+}
+
+// Here is an undefined response.
+
+const undefined = falsy;
+if(undefined){
+    console.log("Undefined is falsy.");
+}
+
+// num0 will be declared false.
+let num0 = 0;
+if(num0){
+    console.log("The number 0 is falsy (the only falsy number).");
+}
+// "" is being declared as false.
+
+let "" = false;
+if(""){
+    console.log("The empty string is falsy(the only falsy string).");
+}
